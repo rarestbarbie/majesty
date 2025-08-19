ae38671e9cfc9a080cc68937dc300bef4a4002c6
Barbie <barbie@barbiefront.com>
2025-08-19 01:50:32 +0000

    enable minimap layers

--- Client/TypeScript/GameUI/Planets/Minimap.ts
+++ Client/TypeScript/GameUI/Planets/Minimap.ts
@@ -1,20 +1,23 @@
 import { GameID } from '../../GameEngine/exports.js';
-import { NavigatorState, HexGrid } from '../exports.js';
+import { NavigatorState, HexGrid, MinimapState, MinimapLayer } from '../exports.js';
 
 export class Minimap {
     readonly node: HTMLDivElement;
     private readonly name: HTMLElement;
     private readonly grid: HexGrid;
+    private readonly layers: HTMLElement;
     private id?: GameID;
 
     constructor() {
         this.name = document.createElement('header');
         this.grid = new HexGrid();
+        this.layers = document.createElement('nav');
 
         this.node = document.createElement('div');
         this.node.id = 'minimap';
         this.node.appendChild(this.name);
         this.node.appendChild(this.grid.node);
+        this.node.appendChild(this.layers);
     }
 
     public update(navigator: NavigatorState): void {
@@ -22,20 +25,49 @@ export class Minimap {
             return;
         }
 
-        this.name.innerText = navigator.minimap.name;
+        const minimap: MinimapState = navigator.minimap;
 
-        const id: GameID = navigator.minimap.id;
+        this.name.innerText = minimap.name;
+
+        const id: GameID = minimap.id;
 
         if (this.id !== id) {
             this.id = id;
             this.grid.switch();
+            this.updateLayerControls(minimap.id, minimap.layer);
+        } else {
+            // Highlight the active layer
+            for (const child of this.layers.children) {
+                const link = child as HTMLAnchorElement;
+                link.classList.toggle('selected', link.dataset.layer === minimap.layer);
+            }
         }
 
         this.grid.update(
-            navigator.minimap.grid,
+            minimap.grid,
             [id],
             (cell: string) => `#planet=${id}&cell=${cell}`,
             navigator.tile?.id,
         );
+    }
+
+    private updateLayerControls(planet: GameID, selected: MinimapLayer): void {
+        this.layers.replaceChildren(); // Clear existing icons
+
+        const icons: Record<MinimapLayer, string> = {
+            [MinimapLayer.Terrain]: '🗺️',
+            [MinimapLayer.Population]: '🧑‍🤝‍🧑',
+            [MinimapLayer.AverageMilitancy]: '😠',
+            [MinimapLayer.AverageConsciousness]: '🤔',
+        };
+
+        for (const layer of Object.values(MinimapLayer)) {
+            const link: HTMLAnchorElement = document.createElement('a');
+            link.href = `#planet=${planet}&layer=${layer}`;
+            link.dataset.layer = layer;
+            link.textContent = icons[layer];
+            link.classList.toggle('selected', layer === selected);
+            this.layers.appendChild(link);
+        }
     }
 }
--- Client/TypeScript/GameUI/Planets/MinimapState.ts
+++ Client/TypeScript/GameUI/Planets/MinimapState.ts
@@ -1,8 +1,9 @@
 import { GameID } from '../../GameEngine/exports.js';
-import { PlanetGridCell } from '../exports.js';
+import { MinimapLayer, PlanetGridCell } from '../exports.js';
 
 export interface MinimapState {
     id: GameID;
     name: string;
     grid: PlanetGridCell[];
+    layer: MinimapLayer;
 }
