9d4164dc19e18aa7daef33dee30304369cc033bb
Barbie <barbie@barbiefront.com>
2025-08-24 01:55:01 +0000

    improve layout of subscreens

--- Client/TypeScript/GameUI/Factories/ProductionOverview.ts
+++ Client/TypeScript/GameUI/Factories/ProductionOverview.ts
@@ -33,6 +33,7 @@ export class ProductionOverview extends ScreenContent {
         readonly index: HTMLUListElement;
         readonly panel: HTMLDivElement;
         readonly stats: HTMLDivElement;
+        readonly nav: HTMLElement;
     };
 
     constructor() {
@@ -65,15 +66,20 @@ export class ProductionOverview extends ScreenContent {
                 index: document.createElement('ul'),
                 panel: document.createElement('div'),
                 stats: document.createElement('div'),
+                nav: document.createElement('nav'),
             }
 
-            this.dom.panel.appendChild(this.dom.stats);
+            const upper: HTMLDivElement = document.createElement('div');
+            upper.appendChild(this.dom.stats);
+            upper.appendChild(this.dom.nav);
+
+            this.dom.panel.appendChild(upper);
             this.dom.panel.appendChild(this.factories.node);
         } else {
             this.dom.stats.replaceChildren();
+            this.dom.nav.replaceChildren();
         }
 
-        const nav: HTMLElement = document.createElement('nav');
         for (const tab of [FactoryDetailsTab.Inventory, FactoryDetailsTab.Ownership]) {
             const link: HTMLAnchorElement = document.createElement('a');
             link.href = `#screen=${ScreenType.Production}&details=${tab}`;
@@ -83,23 +89,26 @@ export class ProductionOverview extends ScreenContent {
                 link.classList.add('selected');
             }
 
-            nav.appendChild(link);
+            this.dom.nav.appendChild(link);
         }
 
         switch (state.factory?.open.type) {
         case FactoryDetailsTab.Inventory:
+            const right: HTMLDivElement = document.createElement('div');
+            right.appendChild(this.sales.node);
+            right.appendChild(this.charts.spending.node);
+
+            this.dom.stats.setAttribute('data-subscreen', 'Inventory');
             this.dom.stats.appendChild(this.needs.node);
-            this.dom.stats.appendChild(this.sales.node);
-            this.dom.stats.appendChild(this.charts.spending.node);
+            this.dom.stats.appendChild(right);
             break;
 
         case FactoryDetailsTab.Ownership:
+            this.dom.stats.setAttribute('data-subscreen', 'Ownership');
             this.dom.stats.appendChild(this.charts.country.node);
             this.dom.stats.appendChild(this.charts.culture.node);
             break;
         }
-
-        this.dom.stats.appendChild(nav);
 
         if (root) {
             root.appendChild(this.dom.index);
