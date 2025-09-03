b5ff2575b8297d478ed4d2ba6439e71f4d48e483
Diana Ma <barbie@barbiefront.com>
2025-09-03 03:03:49 +0000

    add tooltip for output

--- Client/TypeScript/GameUI/ResourceSaleBox.ts
+++ Client/TypeScript/GameUI/ResourceSaleBox.ts
@@ -1,41 +1,70 @@
 import {
     DiffableListElement,
+    Fortune,
+    Ticker,
     UpdateBigInt,
 } from '../DOM/exports.js';
-import { Resource, ResourceSale } from './exports.js';
+import { GameID } from '../GameEngine/exports.js';
+import { Resource, ResourceSale, TooltipType } from './exports.js';
 
 export class ResourceSaleBox implements DiffableListElement<Resource> {
     public readonly id: Resource;
     public readonly node: HTMLAnchorElement;
 
-    private readonly unitsProduced: HTMLElement;
-    private readonly unitsLeftover: HTMLElement;
-    private readonly valueSold: HTMLElement;
+    private readonly units: HTMLElement;
+    private readonly value: HTMLElement;
+    private readonly price: Ticker;
 
-    constructor(sale: ResourceSale) {
+    constructor(
+        sale: ResourceSale,
+        owner: GameID,
+        supply: TooltipType,
+        explainPrice: TooltipType,
+    ) {
         this.id = sale.id;
         this.node = document.createElement('a');
 
         const icon: HTMLDivElement = document.createElement('div');
         const resource: HTMLDivElement = document.createElement('div');
 
-        this.unitsProduced = document.createElement('div');
-        this.unitsLeftover = document.createElement('div');
-        this.valueSold = document.createElement('div');
-
         icon.textContent = sale.icon;
         resource.textContent = sale.name;
 
+        this.units = document.createElement('div');
+        this.units.setAttribute('data-tooltip-type', supply);
+        this.units.setAttribute(
+            'data-tooltip-arguments',
+            JSON.stringify([owner, sale.id]),
+        );
+
+        const proceeds: HTMLDivElement = document.createElement('div');
+
+        this.value = document.createElement('div');
+        this.price = new Ticker(Fortune.Bonus);
+
+        proceeds.appendChild(this.value);
+        proceeds.appendChild(this.price.outer);
+        proceeds.classList.add('proceeds');
+        proceeds.setAttribute('data-tooltip-type', explainPrice);
+        proceeds.setAttribute(
+            'data-tooltip-arguments',
+            JSON.stringify([owner, null, sale.id]),
+        );
+
         this.node.appendChild(icon);
         this.node.appendChild(resource);
-        this.node.appendChild(this.unitsProduced);
-        this.node.appendChild(this.unitsLeftover);
-        this.node.appendChild(this.valueSold);
+        this.node.appendChild(this.units);
+        this.node.appendChild(proceeds);
     }
 
     public update(sale: ResourceSale): void {
-        UpdateBigInt(this.unitsProduced, sale.unitsSold);
-        UpdateBigInt(this.unitsLeftover, sale.unitsProduced - sale.unitsSold);
-        UpdateBigInt(this.valueSold, sale.valueSold);
+        UpdateBigInt(this.units, sale.unitsSold);
+        UpdateBigInt(this.value, sale.valueSold);
+
+        if (sale.priceAtMarket !== undefined) {
+            this.price.updatePriceChange(sale.priceAtMarket.o, sale.priceAtMarket.c, 2);
+        } else if (sale.price !== undefined) {
+            this.price.updateBigIntChange(sale.price.o, sale.price.c);
+        }
     }
 }
