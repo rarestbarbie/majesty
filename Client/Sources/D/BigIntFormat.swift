3dd7e41709af0baa7b380cb894713de550dbe92a
Barbie <barbie@barbiefront.com>
2025-09-07 01:27:03 +0000

    use open source version of d

--- Client/Sources/D/BigIntFormat.swift
+++
@@ -1,7 +0,0 @@
-@frozen public struct BigIntFormat {
-    public let stride: UInt8
-
-    @inlinable init(stride: UInt8) {
-        self.stride = stride
-    }
-}
--- Client/Sources/D/BinaryInteger (ext).swift
+++
@@ -1,5 +0,0 @@
-extension BinaryInteger {
-    @inlinable public subscript(format: BigIntFormat) -> BigIntRepresentation<Self> {
-        .init(value: self, stride: format.stride, signed: false)
-    }
-}
