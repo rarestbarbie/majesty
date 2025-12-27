af2481eccf6a3acad3ae42c762634228218ce477
Barbie <barbie@barbiefront.com>
2025-12-27 17:32:23 +0000

    refactor Gender curves into pure static functions

--- Sources/GameIDs/GenderAuthority.swift
+++ Sources/GameIDs/GenderAuthority.swift
@@ -18,39 +18,3 @@ extension GenderAuthority {
         }
     }
 }
-extension GenderAuthority {
-    @inlinable public func sequestration(recipient: Sex, plurality: Double) -> Double {
-        switch self {
-        case .F:
-            switch recipient {
-            case .F: 0
-            case .X: 0
-            case .M: 1 - plurality
-            }
-        case .XL:
-            switch recipient {
-            case .F: 1 - plurality
-            case .X: 0
-            case .M: 0
-            }
-        case .X:
-            switch recipient {
-            case .F: 1 - plurality
-            case .X: 0
-            case .M: 1 - plurality
-            }
-        case .XG:
-            switch recipient {
-            case .F: 0
-            case .X: 0
-            case .M: 1 - plurality
-            }
-        case .M:
-            switch recipient {
-            case .F: 1 - plurality
-            case .X: 1 - plurality
-            case .M: 0
-            }
-        }
-    }
-}
