0cfe3d5402998245f28b07342f994bea5755af6f
Barbie <barbie@barbiefront.com>
2025-12-28 04:25:45 +0000

    factory utilization

--- Sources/GameEngine/Factories/Factory.Stats.swift
+++ Sources/GameEngine/Factories/Factory.Stats.swift
@@ -2,6 +2,7 @@ import Assert
 
 extension Factory {
     struct Stats {
+        private(set) var utilization: Double
         private(set) var productivity: Int64
         private(set) var cashFlow: CashFlowStatement
         private(set) var profit: ProfitMargins
@@ -9,11 +10,23 @@ extension Factory {
 }
 extension Factory.Stats {
     init() {
-        self.init(productivity: 0, cashFlow: .init(), profit: .undefined)
+        self.init(
+            utilization: 0,
+            productivity: 0,
+            cashFlow: .init(),
+            profit: .undefined
+        )
     }
 }
 extension Factory.Stats {
     mutating func update(from state: Factory, in region: RegionalProperties) {
+        if  let utilization: Double = state.inventory.out.utilization {
+            #assert(0 ... 1 ~= utilization, "Utilization must be between 0 and 1")
+            self.utilization = utilization
+        } else {
+            self.utilization = 1
+        }
+
         self.productivity = region.modifiers.factoryProductivity[state.type]?.value ?? 1
 
         self.cashFlow.reset()
